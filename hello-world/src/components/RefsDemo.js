3beec85c2394b7e713eb2152f53b4dcbb46f38e6
bavshehata <bavly@bavlifyweb.com>
2023-10-09 19:52:03 +0300

    callback refs

--- hello-world/src/components/RefsDemo.js
+++ hello-world/src/components/RefsDemo.js
@@ -2,19 +2,23 @@ import React, { Component } from "react";
 
 export class RefsDemo extends Component {
   inputRef = React.createRef();
-
+  cbRef = null;
+  setCbRef = (ele) => (this.cbRef = ele);
   handleClick = () => {
     alert(this.inputRef.current.value);
   };
+
   componentDidMount() {
-    console.log(this.inputRef);
-    console.log(this.inputRef.current);
-    this.inputRef.current.focus();
+    console.log(this.cbRef);
+    if (this.cbRef) this.cbRef.focus();
+
+    // this.inputRef.current.focus();
   }
   render() {
     return (
       <div>
         <input type="text" ref={this.inputRef} />
+        <input type="text" ref={this.setCbRef} />
         <button onClick={this.handleClick}>Click</button>
       </div>
     );
